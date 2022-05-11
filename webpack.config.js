0ad79bf76d75901fdc2d81b87709965168de4f8d
Sebastian Aanstad <sebbeaanstad@gmail.com>
2022-05-11 18:24:28 +0200

    small stuffs

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,11 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-}
+    entry: {
+      index: './src/index.js',
+    },
+     output: {
+      filename: '[name].bundle.js',
+       path: path.resolve(__dirname, 'quiz-data-pub v0.1'),
+     },
+   };
